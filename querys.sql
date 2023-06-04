449779570469f26d8229128fefe417f0e1a7d7ad
oscarmedina234 <oscarmedina234@gmail.com>
2023-06-04 01:14:32 -0500

    added more querys

--- querys.sql
+++ querys.sql
@@ -5,4 +5,27 @@ JOIN role ON employee.role_id = role.id
 LEFT JOIN department ON role.department_id = department.id
 
 --add employee--
-INSERT INTO 
+INSERT INTO employee (first_name, last_name, role_id)
+Values ();
+
+--update employee role--
+UPDATE employee
+SET role_id = ?
+WHERE id = ?
+
+--view all roles--
+SELECT role.id, role.title, role.salary, department.name
+FROM role
+JOIN department ON role.department_id = department.id
+
+--add role--
+INSERT INTO role (title, salary,  department_id)
+VALUES ();
+
+--view all departments--
+SELECT * FROM departments;
+
+--add department--
+
+INSERT INTO department (name)
+VALUES ();
