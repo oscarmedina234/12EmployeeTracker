b212b6ad5cbe8e26eef9a83572bdc6f565ba9643
oscarmedina234 <oscarmedina234@gmail.com>
2023-06-04 02:29:20 -0500

    added the update role query

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const db = mysql.createConnection(
     },
     console.log('Connected to the company_db database')
 );
-
+// this is the original prompt
 function promptUser() {
 inquirer
   .prompt([
@@ -49,8 +49,7 @@ inquirer
         addEmployeesPrompt();
 
     } else if (menuChoice === 'Update employee role') {
-        console.log('Update employee role');
-        promptUser();
+        updateEmployeeRolePrompt();
 
     } else if (menuChoice === 'View all roles') {
         console.log('View all roles');
@@ -70,7 +69,7 @@ inquirer
     }
   })
 };
-
+// This function is the prompts required for the employee fields
 function addEmployeesPrompt() {
     inquirer
     .prompt([
@@ -96,7 +95,28 @@ function addEmployeesPrompt() {
         addEmployee(firstName, lastName, roleId);
     })
 }
+//this is the prompt to get all the fields required to update the employee role
+function updateEmployeeRolePrompt() {
+    inquirer
+    .prompt([
+        {
+            type: 'input',
+            name:'role_id',
+            message:'What is the new role_id?'
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'Whats is the employees id?'
+        },
+    ]) .then((answers) => {
+        let newRole = answers.role_id;
+        let employId = answers.id;
+        updateEmployeeRole(newRole, employId);
+    })
+}
 
+//this function gets all employees then restarts the original prompt
 function getAllEmployees() {
     db.query('SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary FROM employee JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id', (err, results) => {
         if (err) {
@@ -107,15 +127,28 @@ function getAllEmployees() {
     });
   };
 
+  // this function adds an employee and restarts original prompt
   function addEmployee(firstName, lastName, id) {
     db.query('INSERT INTO employee (first_name, last_name, role_id)Values (?,?,?);',[firstName, lastName, id], (err, results) => {
         if (err) {
             console.log(err);
           }
+          console.log(`${firstName} ${lastName} has been added to the database with the role id ${id}.`)
           promptUser(); 
     })
   };
 
+// this function updates an employees role
+function updateEmployeeRole(newRole,employId) {
+    db.query('UPDATE employee SET role_id = ? WHERE id = ?;',[newRole, employId], (err, results) => {
+        if (err) {
+            console.log(err);
+          }
+          console.log(`The employee with the id ${employId} role has been changed to ${newRole}.`);
+          promptUser(); 
+    })
+  };
+  
 // Default response for any other request (Not Found)
 app.use((req, res) => {
     res.status(404).end();
