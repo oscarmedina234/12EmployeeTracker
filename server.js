384907e6eaea11fa3cd09dd01cc15eb028ee6451
oscarmedina234 <oscarmedina234@gmail.com>
2023-06-03 23:30:05 -0500

    set up my package.json file

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mysql = require('mysql2');
-const inquire = require('inquirer');
+const inquirer = require('inquirer');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
