294435ef438b7a73f0ae5f10b1c85dc1ec2784b6
oscarmedina234 <oscarmedina234@gmail.com>
2023-06-03 22:33:26 -0500

    changed schema

--- schema.sql
+++ schema.sql
@@ -4,25 +4,23 @@ USE company_db;
 
 
 CREATE TABLE department (
-    id INT,
-    name VARCHAR(30),
-    PRIMARY KEY (id)
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30)
 );
 
 CREATE TABLE role (
-    id INT NOT NULL,
-    title VARCHAR(30),
-    salary DECIMAL,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
     department_id INT,
-    PRIMARY KEY (id),
     FOREIGN KEY (department_id)
     REFERENCES department(id)
 );
 
 CREATE TABLE employee (
-    id INT PRIMARY KEY,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
     role_id INT,
     manager_id INT,
     FOREIGN KEY (role_id)
